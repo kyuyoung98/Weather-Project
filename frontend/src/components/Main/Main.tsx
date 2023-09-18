d5ca8357046184d5c428359f0589c4f52c8b9e11
kyuyoung <jky5457@naver.com>
2023-09-18 19:40:29 +0900

    isLogin home or main page available

---
+++ frontend/src/components/Main/Main.tsx
@@ -0,0 +1,12 @@
+import React, { useContext, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Main = () => {
+  return (
+    <div>
+        <h1>이게 메인 이지롱</h1>
+    </div>
+  );
+}
+
+export default Main;
